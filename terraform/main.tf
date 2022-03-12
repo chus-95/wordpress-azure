caebed903fb71c43304218cf1bf7b26633920900
chus-95 <chesprov@gmail.com>
2022-03-12 12:36:43 +0100

    drop outputs and var_azure provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,3 @@
-# crea un service principal y rellena los siguientes datos para autenticar
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 
 terraform {
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_id" {
-    value= azurem_linux_virtual_machine.linux_virtual_machine.id
-}
-
-output "vm_ip" {
-    value = azurem_linux_virtual_machine.linux_virtual_machine.public_ip_address
-}
