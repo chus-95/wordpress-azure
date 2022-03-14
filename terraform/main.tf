932aee74aa40dc0b5fb092a34f167b102d1e311d
chus-95 <chesprov@gmail.com>
2022-03-14 14:42:21 +0100

    skip tls_private_key

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,15 +149,15 @@ resource "azurerm_storage_account" "stAccount" {
 
 }
 
-resource "tls_private_key" "example_ssh" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-}
+#resource "tls_private_key" "example_ssh" {
+#    algorithm = "RSA"
+#    rsa_bits = 4096
+#}
 
-output "tls_private_key" { 
-    value = tls_private_key.example_ssh.private_key_pem 
-    sensitive = true
-}
+#output "tls_private_key" { 
+#    value = tls_private_key.example_ssh.private_key_pem 
+#    sensitive = true
+#}
 
 # Virtual machine creation
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
