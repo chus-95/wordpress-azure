279305338760896cdf8fb58f22465982ff37b319
chus-95 <chesprov@gmail.com>
2022-03-14 18:23:14 +0100

    Check code style

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,16 +149,6 @@ resource "azurerm_storage_account" "stAccount" {
 
 }
 
-#resource "tls_private_key" "example_ssh" {
-#    algorithm = "RSA"
-#    rsa_bits = 4096
-#}
-
-#output "tls_private_key" { 
-#    value = tls_private_key.example_ssh.private_key_pem 
-#    sensitive = true
-#}
-
 # Virtual machine creation
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
 
